fa9321c14277bc5469e7424339e31086706ed980
Miklos Suveges <miklos.suveges@gmail.com>
2020-10-14 17:07:09 +0200

    Initial commit

---
+++ test/rest-api.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as RestApi from '../lib/rest-api-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new RestApi.RestApiStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
