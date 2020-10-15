72472dc3512cfd37954a9e568a278d82f1a5f35c
Chris Schofield <chrisschofield@Chriss-MacBook-Pro.local>
2020-10-15 20:11:14 +0200

    serverSideProps + types

--- frontend/pages/[name].tsx
+++ frontend/pages/[name].tsx
@@ -2,6 +2,16 @@ import * as React from 'react';
 import { useRouter } from 'next/router';
 import { createUseStyles } from 'react-jss';
 import { Theme } from '../utils/theme';
+import Axios from 'axios';
+import { GetServerSidePropsResult, NextPageContext } from 'next';
+
+type Props = {
+  content: string;
+};
+
+type PageProps = {
+  content: string;
+};
 
 const useStyles = createUseStyles(
   (theme: Theme) => ({
@@ -10,6 +20,7 @@ const useStyles = createUseStyles(
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
+      flexDirection: 'column',
       height: `calc(100vh - ${theme.spacing(8)}px)`,
     },
     title: {
@@ -17,19 +28,39 @@ const useStyles = createUseStyles(
       textTransform: 'uppercase',
       fontSize: theme.spacing(6),
     },
+    subtitle: {
+      maxWidth: theme.spacing(60),
+      lineHeight: `${theme.spacing(3.5)}px`,
+      textAlign: 'center',
+    },
   }),
   { name: 'Name' },
 );
 
-const NamePage = (): React.ReactElement => {
+const NamePage = ({ content }: Props): React.ReactElement => {
   const classes = useStyles();
   const router = useRouter();
   const { name } = router.query;
   return (
     <div className={classes.root}>
       <h1 className={classes.title}>👋 Hi {name} 👋</h1>
+      <p className={classes.subtitle}>{content}</p>
     </div>
   );
 };
+
+export async function getServerSideProps(
+  context: NextPageContext,
+): Promise<GetServerSidePropsResult<PageProps>> {
+  console.log(context.query);
+  const { data: content } = await Axios.get(
+    'https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1',
+  );
+  return {
+    props: {
+      content,
+    },
+  };
+}
 
 export default NamePage;
